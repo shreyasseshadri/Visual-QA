7b566370c54392b0e9456582e08c7b86c384b87b
shreyas <shreyasseshadri@gmail.com>
2019-01-11 18:54:41 +0530

    added generator

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -1,9 +1,12 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import tensornets as nets
 import keras
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 import numpy as np
 import os
+import pandas as pd
+
 def text_feature_extraction(corpus):
     '''
     Corpus : A list of Questions in the dataset
@@ -18,7 +21,7 @@ def text_feature_extraction(corpus):
     text_features=np.array(vectorizer.fit_transform(corpus).todense())
     np.save(text_features,"../data/text_features.npy")
 
-def image_feature_extraction(images):
+def image_feature_extraction(df):
     '''
     images: A numpy 4D array of shape (no. of examples,299,299,3)
     '''
@@ -28,8 +31,26 @@ def image_feature_extraction(images):
             return
         else:
             pass
+    file_list = os.listdir('../data/train_images')
+    print(len(file_list))
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_generator = test_datagen.flow_from_dataframe(
+        dataframe=df,
+        directory="../data/train_images/",
+        x_col="image",
+        y_col=None,
+        batch_size=32,
+        class_mode=None,
+        target_size=(229,229))
+    filenames = test_generator.filenames
+    nb_samples = len(filenames)
+    print(nb_samples)
     model=keras.applications.inception_v3.InceptionV3(include_top=True, weights='imagenet', pooling='avg')
-    model.pop()
+    model.layers.pop()
     model = Model(model.input, model.layers[-1].output)
-    image_features=model.predict(images)
-    np.save(image_features,"../data/image_features.npy")        
+    image_features = model.predict_generator(test_generator,steps = np.ceil(nb_samples/64))
+    print(image_features.shape)
+    #np.save(image_features,"../data/image_features.npy")        
+
+data=pd.read_csv("../data/train.csv")
+image_feature_extraction(data)
